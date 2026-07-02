869256b1e27fc02919d6bc886d20c296b9c85dd8
Aryaman <aryaman.pandey@wipro.com>
2026-07-02 16:16:54 +0530

    navbar feature changes

--- src/test/java/features/B_Navbar.feature
+++ src/test/java/features/B_Navbar.feature
@@ -13,3 +13,14 @@ Then User should see navbar logo
 And User should see Offers link
 And User should see Orders link
 And User should see Favourites link
+
+Scenario: Verify search fails for any keyword
+Given User is on BStackDemo HomePage
+When User enters "iphone" in search bar
+And User clicks on Search icon
+Then Search results should not be displayed
+And User should see no matching products message
+
+Scenario: Log Out 
+When User clicks on Log Out
+Then User is Logged Out
--- src/test/java/stepDef/NavbarSteps.java
+++ src/test/java/stepDef/NavbarSteps.java
@@ -1,18 +1,22 @@
 package stepDef;
  
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+
 import Hooks.BStackDemoHooks;
+import PageObjectsModels.HomePage;
 import PageObjectsModels.NavBar;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
-import org.testng.Assert;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import io.cucumber.java.en.When;
  
-public class NavbarLinks {
-	private static final Logger logger = LogManager.getLogger(NavbarLinks.class);
+public class NavbarSteps {
+	private static final Logger logger = LogManager.getLogger(NavbarSteps.class);
  
     NavBar nb = new NavBar(BStackDemoHooks.driver);
- 
+    HomePage hp=new HomePage(BStackDemoHooks.driver);
+    
     @Then("User should see navbar logo")
     public void navbarLogo() {
     	logger.info("Verifying navbar logo visibility");
@@ -57,5 +61,45 @@ public class NavbarLinks {
         BStackDemoHooks.driver.navigate().back();
         logger.info("Returned back to Home Page");
     }
+    
+    @When("User enters {string} in search bar")
+    public void user_enters_keyword(String keyword) {
+        logger.info("Entering keyword in search bar: " + keyword);
+        nb.searchBar().clear();
+        nb.searchBar().sendKeys(keyword);
+    }
+ 
+    @And("User clicks on Search icon")
+    public void searchIcon() {
+        logger.info("Clicking on Search icon");
+        nb.searchButton().click();
+    }
+ 
+    @Then("Search results should not be displayed")
+    public void verifySearchResultsNotDisplayed() {
+        // INTENTIONAL FAILURE – BUG LOG
+        logger.error("BUG: Search functionality does not filter products based on keyword entered");
+ 
+        Assert.fail("BUG: Search functionality does not filter products based on keyword entered");
+    }
+ 
+    @And("User should see no matching products message")
+    public void verifyNoMatchingProductsMessage() {
+        logger.warn("No matching products message not displayed – expected behavior");
+        Assert.assertTrue(true);
+    }
+    
+    @When("User clicks on Log Out")
+    public void clickLogOut() {
+    	logger.info("Clicking on Log Out");
+    	nb.logout().click();
+    	
+    }
+    
+    @Then("User is Logged Out")
+    public void verifyLogOut() {
+    	Assert.assertTrue(hp.SignInLink().isDisplayed());
+    }
+    
 }
  
