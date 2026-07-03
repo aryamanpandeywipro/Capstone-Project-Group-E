a31ad81c307230baed1147b65fd0e9d4c4156371
Chanchal Bhangale <ch20695935@wipro.com>
2026-07-03 16:26:55 +0530

    Implemented log and extent and comments

--- src/test/java/Runner/Runner.java
+++ src/test/java/Runner/Runner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-		features = "src/test/java/features",
+		features = "src/test/java/features/B_Navbar.feature",
 		glue={"stepDef", "Hooks"}, monochrome=true,
 		plugin= {"pretty","html:target/CapstoneReport.html"}
 		)
